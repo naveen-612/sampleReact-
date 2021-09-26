ef274a75f9a1788df689a6af199fb1bfc13cf558
naveen-612 <amailtonavi.email>
2021-09-26 14:40:14 +0200

    added menu button

---
+++ my-react/src/App.js
@@ -0,0 +1,36 @@
+// import logo from './logo.svg';
+import './App.css';
+import Iris from '../src/images/iris.jpg';
+import Select from '../src/components/menu/menu';
+
+import Edit from './.././src'
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header  */}
+      {/* // className="App-header"> */}
+        {/* <img src={Iris} className="App-logo" alt="logo" /> */}
+      
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+
+
+      {/* </header> */}
+
+      <div > 
+
+        <Select />
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ my-react/src/components/menu/menu.js
@@ -0,0 +1,36 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import  React,{useState} from 'react'
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+export default function select() {
+    const [age, setAge] = useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+    return (
+        <div style ={{width:"50%"}}>
+             <Box  style ={{width:"50%"}}>
+
+      <FormControl >
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          label="Age"
+          onChange={handleChange}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+        </div>
+    )
+}
